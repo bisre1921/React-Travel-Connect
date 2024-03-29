1e065baee79c950986f5b8cac2a04a400710110c
bisre1921 <bisrattewodros3@gmail.com>
2024-03-29 14:34:14 +0300

    create header and navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import {BrowserRouter as Router , Routes , Route , Navigate} from "react-router-dom"
 import Users from "./users/pages/Users"
 import NewPlace from "./places/pages/NewPlace"
+import MainNavigation from "./shared/components/Navigation/MainNavigation"
 function App() {
 
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/places/new" element={<NewPlace />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
       </Router>
     </>
   )
