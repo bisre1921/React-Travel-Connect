dd9ec45a0ab4a4aa3c248b3e0559ce34ae733fa9
bisre1921 <bisrattewodros3@gmail.com>
2024-04-12 21:17:25 +0300

    Implementing state management for input values and enabling support for multiple input fields

--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer , useEffect } from "react";
 import "./Input.css";
 import { validate } from "../../Util/Validators";
 
@@ -23,6 +23,13 @@ const InputReducer = (state , action) => {
 const Input = (props) => {
 
     const [inputState , dispatch] = useReducer(InputReducer , {value : "" , isValid : false , isTouched : false});
+    const {id , onInput} = props;
+    const {value , isValid} = inputState;
+
+    useEffect(() => {
+        onInput(id , value , isValid)
+    } , [id , value , isValid , onInput]);
+
 
     const changeHandler = event => {
         dispatch({type : "CHANGE" , val : event.target.value , validators : props.validators})
