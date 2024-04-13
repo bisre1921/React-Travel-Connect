9fed7eb8238e5900130e3fa4188b9360c8f90a9e
bisre1921 <bisrattewodros3@gmail.com>
2024-04-13 15:27:16 +0300

    start work on the updatePlace page

---
+++ src/places/pages/UpdatePlace.jsx
@@ -0,0 +1,11 @@
+import {useParams} from "react-router-dom";
+
+const UpdatePlace = () => {
+    const placeId = useParams().placeId
+    return (
+        <div>
+            hello
+        </div>
+    )
+};
+export default UpdatePlace;
