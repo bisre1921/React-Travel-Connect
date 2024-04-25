4b4ac6ea9ae58e330456c44f07e998ddad5413b5
bisre1921 <bisrattewodros3@gmail.com>
2024-04-25 20:41:43 +0300

    get users from the database

--- Backend/Controllers/UsersControllers.jsx
+++ Backend/Controllers/UsersControllers.jsx
@@ -12,7 +12,16 @@ const dummyUsers = [
     }
 ]
 
-const getUsers = (req, res , next) => {
+const getUsers = async (req, res , next) => {
+    let users;
+    try {
+        users = User.find({} , "-password");
+    } catch (err) {
+        const error = new HttpError("Fetching users failed, please try again later" , 500);
+        return next(error);
+    };
+    
+
     res.json({users : dummyUsers});
 };
 
