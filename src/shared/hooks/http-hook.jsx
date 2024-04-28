7d1375b5334c6c3518f83881d1b53681193aaff2
bisre1921 <bisrattewodros3@gmail.com>
2024-04-28 18:55:38 +0300

    improve the custom http hook

--- src/shared/hooks/http-hook.jsx
+++ src/shared/hooks/http-hook.jsx
@@ -1,26 +1,57 @@
-import { useState } from "react"
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useCallback, useState } from "react"
 
 export const useHttpClient = () => {
     const [isLoading , setIsLoading] = useState(false);
     const [error , setError] = useState();
 
-    const sendRequest = async (url , method = "GET" , body = null , headers = {}) => {
+    const activeHttpRequests = useRef([]);
+
+    const sendRequest = useCallback(async (url , method = "GET" , body = null , headers = {}) => {
+        setIsLoading(true);
+        const httpAbortCtrl = new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrl);
         try {
             const response = await fetch(url , {
                 method,
                 body,
-                headers
+                headers, 
+                signal: httpAbortCtrl.signal
             });
     
             const responseData = await response.json();
+
+            activeHttpRequests.current = activeHttpRequests.current.filter(
+                reqCtrl => reqCtrl !== httpAbortCtrl
+            );
     
             if(!response.ok) {
                 throw new Error(responseData.message);
             };
+
+            setIsLoading(false);
+            return responseData;
         } catch (error) {
             setError(error.message);
+            setIsLoading(false);
+            throw error;
         }
+        
       
+    } , []);
 
-    };
+    const clearError = () => {
+        setError(null);
+    }
+
+    useEffect(() => {
+        return () => {
+            activeHttpRequests.current.forEach(abortctrl => abortctrl.abort());
+        }
+    } , [])
+
+    return (
+        {isLoading , error , sendRequest , clearError}
+    )
 } 
