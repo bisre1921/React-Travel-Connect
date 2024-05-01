75a1a03038ebc9617896d61b5908b261d8d962da
bisre1921 <bisrattewodros3@gmail.com>
2024-05-01 22:45:13 +0300

    implement the functionality of wiring frotnend and backend up

--- Backend/middleware/file-upload.jsx
+++ Backend/middleware/file-upload.jsx
@@ -15,10 +15,10 @@ const fileUpload = multer({
         } ,
         filename : (req , file , cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype];
-            cb(null , uuid() + "." + ext);
+            cb(null , uuidv4() + "." + ext);
         }
     }) ,
-    fileFilter : (req , res , cb) => {
+    fileFilter : (req , file , cb) => {
         const isValid = !!MIME_TYPE_MAP[file.mimetype];
         let error = isValid ? null : new Error("Invalid mime type");
         cb(error , isValid);
