938ae390ef58430acac4c5bfd47183530d261811
bisre1921 <bisrattewodros3@gmail.com>
2024-05-01 21:52:47 +0300

    filter files on the backend images only

--- Backend/middleware/file-upload.jsx
+++ Backend/middleware/file-upload.jsx
@@ -1,5 +1,5 @@
 const multer = require("multer");
-const uuid = require("uuid/v1");
+const { v4: uuidv4 } = require('uuid');
 
 const MIME_TYPE_MAP = {
     "image/png" : "png" , 
@@ -11,13 +11,19 @@ const fileUpload = multer({
     limits: 500000 , 
     storage : multer.diskStorage({
         destination: (req , file , cb) => {
-
+            cb(null , "uploads/images")
         } ,
         filename : (req , file , cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype];
             cb(null , uuid() + "." + ext);
         }
-    })
+    }) ,
+    fileFilter : (req , res , cb) => {
+        const isValid = !!MIME_TYPE_MAP[file.mimetype];
+        let error = isValid ? null : new Error("Invalid mime type");
+        cb(error , isValid);
+    }
+
 });
 
 module.exports = fileUpload;
