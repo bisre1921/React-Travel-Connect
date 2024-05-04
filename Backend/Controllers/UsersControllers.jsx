46afca885093ca4fb69eda9247bf01d630480e1b
bisre1921 <bisrattewodros3@gmail.com>
2024-05-04 11:24:29 +0300

    generate tokens JWT on the backend

--- Backend/Controllers/UsersControllers.jsx
+++ Backend/Controllers/UsersControllers.jsx
@@ -2,6 +2,7 @@ const HttpError = require("../Models/HttpError.jsx");
 const {validationResult} = require("express-validator");
 const User = require("../Models/user.jsx");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 const getUsers = async (req, res , next) => {
@@ -64,8 +65,24 @@ const signup = async (req, res , next) => {
         return next(error);
     };
 
+    let token;
+    try {
+        token = jwt.sign(
+            {
+                userId : createdUser.id , 
+                email : createdUser.email
+            } , 
+            "supersecret_dont_share" , 
+            {expiresIn : "1h"} , 
+    
+        );
+    } catch (err) {
+        const error = new HttpError("Signing up failed, please try again" , 500);
+        return next(error);
+    }
+   
 
-    res.status(201).json({user : createdUser.toObject({getters : true})});
+    res.status(201).json({userId : createdUser.id , email : createdUser.email , token : token});
 };
 
 const login = async (req, res , next) => {
@@ -98,9 +115,25 @@ const login = async (req, res , next) => {
         const error = new HttpError("Invalid credentials, Could not log you in" , 401);
         return next(error);
     }
+
+    let token;
+    try {
+        token = jwt.sign(
+            {
+                userId : existingUser.id , 
+                email : existingUser.email
+            } , 
+            "supersecret_dont_share" , 
+            {expiresIn : "1h"} , 
+    
+        );
+    } catch (err) {
+        const error = new HttpError("logging up failed, please try again" , 500);
+        return next(error);
+    }
     
     
-    res.json({message : "Logged in" , user: existingUser.toObject({getters: true})});
+    res.json({userId : existingUser.id , email : existingUser.email , token : token});
 };
 
 
