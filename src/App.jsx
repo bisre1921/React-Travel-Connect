9bb667ca659042a7ca6c381139a2649ad12877e3
bisre1921 <bisrattewodros3@gmail.com>
2024-05-07 13:47:51 +0300

    create a custom Authentication Hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,57 @@ import UserPlaces from "./places/pages/UserPlaces"
 import UpdatePlace from "./places/pages/UpdatePlace"
 import Auth from "./users/pages/Auth"
 import  AuthContext  from "./shared/Context/AuthContext"
-import { useEffect, useState } from "react"
-import { useCallback } from "react"
+import { useAuth } from "./shared/hooks/Auth-Hook"
 
-let logoutTimer;
 
 function App() {
 
-  const [token , setToken] = useState(false);
-  const [tokenExpirationDate , setTokenExpirationDate] = useState();
-  const [userId , setUserId] = useState(false);
+  const {token , login , logout , userId} = useAuth();
+
+  // const [token , setToken] = useState(false);
+  // const [tokenExpirationDate , setTokenExpirationDate] = useState();
+  // const [userId , setUserId] = useState(false);
 
  
-  const login = useCallback((uid , token , expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExpirationDate);
+  // const login = useCallback((uid , token , expirationDate) => {
+  //   setToken(token);
+  //   setUserId(uid);
+  //   const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+  //   setTokenExpirationDate(tokenExpirationDate);
 
-    localStorage.setItem(
-      "userData" , 
-      JSON.stringify({
-        userId : uid , 
-        token : token ,
-        expiration : tokenExpirationDate.toISOString()
-      })
-    );
+  //   localStorage.setItem(
+  //     "userData" , 
+  //     JSON.stringify({
+  //       userId : uid , 
+  //       token : token ,
+  //       expiration : tokenExpirationDate.toISOString()
+  //     })
+  //   );
 
-  } , []);
+  // } , []);
 
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  } , []);
+  // const logout = useCallback(() => {
+  //   setToken(null);
+  //   setTokenExpirationDate(null);
+  //   setUserId(null);
+  //   localStorage.removeItem("userData");
+  // } , []);
 
-  useEffect(() => {
-    if(token && tokenExpirationDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout , remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  } , [token , logout , tokenExpirationDate]);
+  // useEffect(() => {
+  //   if(token && tokenExpirationDate) {
+  //     const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+  //     logoutTimer = setTimeout(logout , remainingTime);
+  //   } else {
+  //     clearTimeout(logoutTimer);
+  //   }
+  // } , [token , logout , tokenExpirationDate]);
 
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if(storedData && storedData.token && new Date(storedData.expiration) > new Date() ) {
-      login(storedData.userId , storedData.token , new Date(storedData.expiration));
-    }
-  } , [login]);
+  // useEffect(() => {
+  //   const storedData = JSON.parse(localStorage.getItem("userData"));
+  //   if(storedData && storedData.token && new Date(storedData.expiration) > new Date() ) {
+  //     login(storedData.userId , storedData.token , new Date(storedData.expiration));
+  //   }
+  // } , [login]);
 
 
   let routes;
